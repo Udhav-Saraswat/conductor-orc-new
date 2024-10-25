32757f9c337792c798025c69c1ad24d95d6b2192
Udhav Saraswat <udhav.saraswat.17@gmail.com>
2024-10-26 01:21:04 +0530

    DEV : Azure AD integration to conductor UI

--- ui/src/index.js
+++ ui/src/index.js
@@ -10,6 +10,13 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { getBasename } from "./utils/helpers";
 
+// import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
+
+import { MsalAuthenticationTemplate } from "@azure/msal-react";
+import { InteractionType } from "@azure/msal-browser";
+
+// refer https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -19,18 +26,41 @@ const queryClient = new QueryClient({
   },
 });
 
+export function Example() {
+  const authRequest = {
+      scopes: ["openid", "profile"]
+  };
+
+  
+function ErrorComponent({error}) {
+  return <p>An Error Occurred: {error}</p>;
+}
+
+function LoadingComponent() {
+  return <p>Authentication in progress...</p>;
+}
+
+
 ReactDOM.render(
   //<React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <BrowserRouter basename={getBasename()}>
-        <CssBaseline />
-        <ReactQueryDevtools initialIsOpen={true} />
+  <MsalAuthenticationTemplate 
+  interactionType={InteractionType.Popup} 
+  authenticationRequest={authRequest} 
+  errorComponent={ErrorComponent} 
+  loadingComponent={LoadingComponent}
+>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <BrowserRouter basename={getBasename()}>
+          <CssBaseline />
+          <ReactQueryDevtools initialIsOpen={true} />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
 
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
-  </QueryClientProvider>,
+    </MsalAuthenticationTemplate>,
+
   //</React.StrictMode>
   document.getElementById("root")
 );
@@ -39,3 +69,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+}
